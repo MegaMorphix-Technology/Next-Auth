8175f9cbdd3eeb9e39ae48ecb5348288eef8d4a1
raihanuldev <rmmahi4770@gmail.com>
2024-02-06 22:16:51 +0600

    profile info added

--- src/pages/profile.js
+++ src/pages/profile.js
@@ -2,7 +2,11 @@ import { Avatar } from "antd";
 import { UserOutlined } from "@ant-design/icons";
 
 import Head from "next/head";
+import { useSession } from "next-auth/react";
+
 const ProfilePage = () => {
+  const {data:session} = useSession();
+
   return (
     <div style={{ textAlign: "center" }}>
       <Head>
@@ -10,6 +14,10 @@ const ProfilePage = () => {
       </Head>
       <h1>User Profile</h1>
       <Avatar size={64} icon={<UserOutlined />} />
+      <div>
+        <h1 style={{textAlign:"center",fontFamily:"sans-serif",marginTop:"10%"}}> User Name: {session?.user?.name} </h1>
+        <p style={{textAlign:"center",fontFamily:"sans-serif"}}> User Email: {session?.user?.email} </p>
+      </div>
     </div>
   );
 };
